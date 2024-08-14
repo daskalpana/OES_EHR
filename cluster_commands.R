ca5569301958d0b2f3bbee03cd619d79006533a4
daskalpana <147017278+daskalpana@users.noreply.github.com>
2024-08-14 08:32:37 -0400

    Add files via upload

---
+++ cluster_commands.R
@@ -0,0 +1,38 @@
+# pwd
+# cd Estimation
+# sbatch binding.SLURM
+# sq
+
+setwd("/home/kalpanad/Estimation")
+case1_results <- read.csv("case1_results.csv")
+case1_results$source <- "1"
+case2_results <- read.csv("case2_results.csv")
+case2_results$source <- "2"
+case3_results <- read.csv("case3_results.csv")
+case3_results$source <- "3"
+case4_results <- read.csv("case4_results.csv")
+case4_results$source <- "4"
+case5_results <- read.csv("case5_results.csv")
+case5_results$source <- "5"
+case6_results <- read.csv("case6_results.csv")
+case6_results$source <- "6"
+case7_results <- read.csv("case7_results.csv")
+case7_results$source <- "7"
+case8_results <- read.csv("case8_results.csv")
+case8_results$source <- "8"
+case9_results <- read.csv("case9_results.csv")
+case9_results$source <- "9"
+case10_results <- read.csv("case10_results.csv")
+case10_results$source <- "10"
+case11_results <- read.csv("case11_results.csv")
+case11_results$source <- "11"
+case12_results <- read.csv("case12_results.csv")
+case12_results$source <- "12"
+case13_results <- read.csv("case13_results.csv")
+case13_results$source <- "13"
+case14_results <- read.csv("case14_results.csv")
+case14_results$source <- "14"
+case15_results <- read.csv("case15_results.csv")
+case15_results$source <- "15"
+case16_results <- read.csv("case16_results.csv")
+case16_results$source <- "16"
