cbe1277910dee929c7609b6d174cb2bd2719b068
daskalpana <147017278+daskalpana@users.noreply.github.com>
2024-08-14 16:11:24 -0400

    Add files via upload

---
+++ special cases/cases.R
@@ -0,0 +1,19 @@
+# case 1 - perfect Z and D; no selection bias
+# case 2 - perfect Z and D; selection via D
+# case 3 - perfect Z and D; selection via Z
+# case 4 - perfect Z and D; selection via D and Z
+
+# case 5 - perfect Z, misclassified D, no selection bias
+# case 6 - perfect Z, misclassified D, selection via D
+# case 7 - perfect Z, misclassified D, selection via Z
+# case 8 - perfect Z, misclassified D, selection via D and Z
+
+# case 9 - misclassified Z, perfect D, no selection bias
+# case 10 - misclassified Z, perfect D, selection via D
+# case 11 - misclassified Z, perfect D, selection via Z
+# case 12 - misclassified Z, perfect D, selection via D and Z
+
+# case 13 - misclassified Z and D, no selection bias
+# case 14 - misclassified Z and D, selection via D
+# case 15 - misclassified Z and D, selection via Z
+# case 16 - misclassified Z and D, selection via D and Z
